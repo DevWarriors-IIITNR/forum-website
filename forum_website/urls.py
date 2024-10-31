58670222131930595778ea7b962567684895b33e
animesh24100 <animesh24100@iiitnr.edu.in>
2024-10-31 13:42:37 +0530

    Set up basic redirects for anonymous users

--- forum_website/urls.py
+++ forum_website/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("test/", views.test, name="about"),
     path("newpost/", views.newpost, name="newpost"),
+    path("signin/", views.pleasesignin, name="signin"),
 ]
