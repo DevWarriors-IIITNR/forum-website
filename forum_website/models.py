e764f095eb3e6b28908e0d940ea187b218a78f8f
animesh24100 <animesh24100@iiitnr.edu.in>
2024-11-01 01:42:11 +0530

    Create Models to test stuff

--- forum_website/models.py
+++ forum_website/models.py
@@ -1,19 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 
-class Thread(models.Model):
-    title = models.CharField()
-    description = models.TextField()
-    sticky = models.BooleanField(default=False)
-    created_at = models.DateTimeField()
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ID = models.AutoField(primary_key=True)
+    picture = models.URLField()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Post(models.Model):
+    ID = models.AutoField(primary_key=True)
+    title = models.CharField()
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET("[deleted]"))
+    created_at = models.DateTimeField()
+
+
+class Comment(models.Model):
+    ID = models.AutoField(primary_key=True)
     title = models.CharField()
     body = models.TextField()
-    thread = models.ForeignKey(Thread, on_delete=models.DO_NOTHING)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    thread = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET("[deleted]"))
     created_at = models.DateTimeField()
