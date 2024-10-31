4ad74fc033d8b49726fbbe7fc6112554f423f3b9
animesh24100 <animesh24100@iiitnr.edu.in>
2024-11-01 02:14:01 +0530

    Implement google login

--- forum_website/urls.py
+++ forum_website/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    path("accounts/", include("allauth.urls")),
     path("about/", views.about, name="about"),
     path("newpost/", views.newpost, name="newpost"),
     path("signin/", views.pleasesignin, name="signin"),
