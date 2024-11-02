b480f6df6a701ad67d0366aa8534caeddd957d8a
animesh24100 <animesh24100@iiitnr.edu.in>
2024-11-03 00:46:11 +0530

    Cleanup

--- forum_website/settings.py
+++ forum_website/settings.py
@@ -99,9 +99,8 @@ WSGI_APPLICATION = "forum_website.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Forum_data",
-        "USER": "",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
