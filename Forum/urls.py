dba42bdfe8c15591a099b0d9a6bf9499c7186196
animesh24100 <animesh24100@iiitnr.edu.in>
2024-11-02 16:24:46 +0530

    Add dynamic colorscheme using prefers-color-scheme

--- Forum/urls.py
+++ Forum/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("newpost/", views.newpost, name="newpost"),
     path("signin/", views.pleasesignin, name="signin"),
     path("logout/", views.logout_view, name="logout"),
+    path("you_are_banned/", views.banned_view, name="banned"),
     path("viewpost/<int:pk>", views.viewpost, name="viewpost"),
     path("deletepost/<int:pk>", views.delete_post, name="delete_post"),
-    #    path("deletecomment/<int:pk>", views.delete_comment, name="delete_comment"),
+    path("deletecomment/<int:pk>", views.delete_comment, name="delete_comment"),
 ]
