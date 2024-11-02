f2c0a603a6c05f94bca5d9aad1bdc7ae76548beb
animesh24100 <animesh24100@iiitnr.edu.in>
2024-11-02 17:57:45 +0530

    edit error in views

--- Forum/views.py
+++ Forum/views.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from django.http.response import HttpResponseNotFound
 from django.shortcuts import HttpResponse, redirect, render
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from .models import Post, Comment, PostForm, CommentForm
 from allauth.socialaccount.models import SocialAccount
 
@@ -16,11 +18,11 @@ def home(request):
     return render(request, "index.html")
 
 
+@login_required
+@require_http_methods(["DELETE"])
 def delete_post(request, pk):
     post = Post.objects.get(pk=pk)
-    if request.user.is_authenticated and (
-        post.user == request.user or request.user.is_staff
-    ):
+    if post.user == request.user or request.user.is_staff:
         post.is_deleted = True
         post.save()
         posts = Post.objects.filter(is_deleted=False).order_by("-created_at")
@@ -31,8 +33,9 @@ def about(request):
     return render(request, "about.html")
 
 
+@login_required
 def newpost(request):
-    if request.user.is_authenticated and not (request.user.profile.is_banned):
+    if not (request.user.profile.is_banned):
         form = PostForm(request.POST)
         if request.method == "POST":
             if form.is_valid():
@@ -44,10 +47,8 @@ def newpost(request):
         else:
             form = PostForm()
         return render(request, "newpost.html", {"form": form})
-    elif request.user.is_authenticated and request.user.profile.is_banned:
-        return redirect(banned_view)
     else:
-        return redirect(pleasesignin)
+        return redirect(banned_view)
 
 
 def pleasesignin(request):
@@ -59,13 +60,15 @@ def pleasesignin(request):
         return HttpResponseNotFound()
 
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect("/")
 
 
+@login_required
 def viewpost(request, pk):
-    if request.user.is_authenticated and not (request.user.profile.is_banned):
+    if not request.user.profile.is_banned:
         form = CommentForm(request.POST)
         post = Post.objects.get(pk=pk)
         comments = Comment.objects.filter(post=post, is_deleted=False).order_by(
@@ -89,17 +92,15 @@ def viewpost(request, pk):
             "viewpost.html",
             {"form": form, "comments": comments, "post": Post.objects.get(pk=pk)},
         )
-    elif request.user.is_authenticated and request.user.profile.is_banned:
-        return redirect(banned_view)
     else:
-        return redirect(pleasesignin)
+        return redirect(banned_view)
 
 
+@login_required
+@require_http_methods(["DELETE"])
 def delete_comment(request, pk):
     comment = Comment.objects.get(pk=pk)
-    if request.user.is_authenticated and (
-        comment.user == request.user or request.user.is_staff
-    ):
+    if comment.user == request.user or request.user.is_staff:
         comment.is_deleted = True
         comment.save()
         comments = Comment.objects.filter(is_deleted=False, post=comment.post).order_by(
@@ -113,8 +114,9 @@ def delete_comment(request, pk):
         )
 
 
+@login_required
 def banned_view(request):
-    if request.user.is_authenticated and request.user.profile.is_banned:
+    if request.user.profile.is_banned:
         return render(request, "youarebanned.html")
     else:
-        return HttpResponseNotFound()
+        return redirect("/")
