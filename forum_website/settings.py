b97729b7711dc399b80f5781f133681fa6075089
animesh24100 <animesh24100@iiitnr.edu.in>
2024-11-02 23:30:43 +0530

    set up basic postgres integration

--- forum_website/settings.py
+++ forum_website/settings.py
@@ -99,8 +99,9 @@ WSGI_APPLICATION = "forum_website.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Forum_data",
+        "USER": "",
     }
 }
 
